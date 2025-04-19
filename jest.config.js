ab49e9031bd0164f4b5227729fb840ec6084ea5f
whoisclebs <43012757+whoisclebs@users.noreply.github.com>
2025-04-18 23:03:52 -0300

    chore: update package.json and tsconfig.json
    - Replaced ESLint with Biome for linting and formatting in package.json.
    
    - Added @biomejs/biome as a dev dependency.
    
    - Updated author field in package.json.
    
    - Cleaned up formatting in package.json and tsconfig.json for consistency.

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,13 @@
 module.exports = {
-  roots: ['<rootDir>/tests'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
-  ],
-  testEnvironment: 'node',
-  coverageProvider: 'babel',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '@/tests/(.*)': '<rootDir>/tests/$1',
-    '@/(.*)': '<rootDir>/src/$1'
-  }
-}
+	roots: ["<rootDir>/tests"],
+	collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
+	testEnvironment: "node",
+	coverageProvider: "babel",
+	transform: {
+		".+\\.ts$": "ts-jest",
+	},
+	moduleNameMapper: {
+		"@/tests/(.*)": "<rootDir>/tests/$1",
+		"@/(.*)": "<rootDir>/src/$1",
+	},
+};
