a6bd262b708034e2a67c0c5659e9a5cb0e21bd79
whoisclebs <43012757+whoisclebs@users.noreply.github.com>
2025-04-20 16:44:32 -0300

    feat: use environment variable for server port configuration

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,8 +1,10 @@
+import config from "@/main/config/env";
+
 const startServer = async () => {
 	const { setupApp } = await import("./config/app");
 	const app = await setupApp();
 	try {
-		await app.listen({ port: 3000 });
+		await app.listen({ port: config.PORT });
 	} catch (err) {
 		app.log.error(err);
 		process.exit(1);
